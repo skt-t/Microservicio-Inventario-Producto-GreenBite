19e97cd3cc5a70eb3c036c2c2beb8debe76ac38a
Scott <scott@skt-t.corylvke.cs@gmail.com>
2026-05-11 19:52:42 -0400

    fix: arreglo complementario de bff

--- microservicio-inventario/src/main/java/com/greenbite/inventario/service/ProductoService.java
+++ microservicio-inventario/src/main/java/com/greenbite/inventario/service/ProductoService.java
@@ -6,25 +6,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service // ¡Esta anotación es clave para Spring!
+@Service 
 public class ProductoService {
 
     private final ProductoRepository repository;
 
-    // Inyección de dependencias
     public ProductoService(ProductoRepository repository) {
         this.repository = repository;
     }
 
-    // Lógica para buscar
     public List<Producto> buscarPorNombre(String nombre) {
-        // Aquí podrías agregar validaciones extra en el futuro (ej. si el nombre está vacío)
         return repository.findByNombreContainingIgnoreCase(nombre);
     }
 
-    // Lógica para guardar
     public Producto guardarProducto(Producto producto) {
-        // Podrías validar que el precio no sea negativo antes de guardar
         return repository.save(producto);
+    }
+
+    // AGREGA ESTO: Lógica para devolver todo el catálogo
+    public List<Producto> listarTodos() {
+        return repository.findAll(); // findAll() ya viene regalado por JpaRepository
     }
 }
